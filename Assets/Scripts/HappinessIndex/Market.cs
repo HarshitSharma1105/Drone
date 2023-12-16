5feb2e0fda8c2cbfb9cee4e3ae2e8fd2180c2546
AryanVerma212 <aryanverma212@outlook.com>
2023-12-16 16:20:32 +0530

    Minor change in Market

--- Assets/Scripts/HappinessIndex/Market.cs
+++ Assets/Scripts/HappinessIndex/Market.cs
@@ -2,10 +2,13 @@ using UnityEngine;
 
 public class Market : MonoBehaviour
 {
+
+    public int marketLevel=1;
     // Start is called before the first frame update
     void Start()
     {
-        
+        God.getUpdate=true;
+        gameObject.tag="Market";
     }
 
     // Update is called once per frame
